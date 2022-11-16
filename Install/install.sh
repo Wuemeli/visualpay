ee6215866f52dc3443df5db88736cb53d7fdfebf
Wuemeli <wuemeli@gmail.com>
2022-11-16 10:14:37 +0000

    Made an MySQL Auto Create

--- Install/install.sh
+++ Install/install.sh
@@ -13,3 +13,59 @@ git@github.com:Wuemeli/visualpay.git
 echo "To complete installation of 'VisualPay' edit the Config PHP File with your Database Informations";
 echo "Then use the pre-made MySQL Scheme that you find in the install Folder";
 
+//Creating the Database and Importing the Scheme
+#!/bin/bash
+# If /root/.my.cnf exists then it won't ask for root password
+if [ -f /root/.my.cnf ]; then
+	echo "Please enter the MySQL database CHARACTER SET! (example: latin1, utf8, ...)"
+	echo "Enter utf8 if you don't know what you are doing"
+	read charset
+	echo "Creating new MySQL database..."
+	mysql -e "CREATE DATABASE VisualPay /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	echo "Database successfully created!"
+	echo "Showing existing databases..."
+	mysql -e "show databases;"
+	echo ""
+	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
+	read username
+	echo "Please enter the PASSWORD for the new MySQL database user!"
+	echo "Note: password will be hidden when typing"
+	read -s userpass
+	echo "Creating new user..."
+	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	echo "User successfully created!"
+	echo ""
+	echo "Granting ALL privileges on ${dbname} to ${username}!"
+	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -e "FLUSH PRIVILEGES;"
+	echo "You're good now :)"
+	exit
+# If /root/.my.cnf doesn't exist then it'll ask for root password	
+else
+	echo "Please enter root user MySQL password!"
+	echo "Note: password will be hidden when typing"
+	read -s rootpasswd
+	echo "Please enter the MySQL database CHARACTER SET! (example: latin1, utf8, ...)"
+	echo "Enter utf8 if you don't know what you are doing"
+	read charset
+	echo "Creating new MySQL database..."
+	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE VisualPay /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	echo "Database successfully created!"
+	echo "Showing existing databases..."
+	mysql -uroot -p${rootpasswd} -e "show databases;"
+	echo ""
+	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
+	read username
+	echo "Please enter the PASSWORD for the new MySQL database user!"
+	echo "Note: password will be hidden when typing"
+	read -s userpass
+	echo "Creating new user..."
+	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	echo "User successfully created!"
+	echo ""
+	echo "Granting ALL privileges on ${dbname} to ${username}!"
+	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+	echo "You're good now :)"
+	exit
+fi
