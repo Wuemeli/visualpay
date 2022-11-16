5f4190ecfc4238542db568f82499a0ae94d00aee
Wuemeli <wuemeli@gmail.com>
2022-11-16 10:05:06 +0000

    Update

--- Install/install.sh
+++ Install/install.sh
@@ -1,11 +1,13 @@
 //DO NOT USE IT NOW ITS IN PROGRESS
 set -e;
 echo "Installing Dependencies";
-apt install composer -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install composer -y
+sudo apt-get install git-all -y
 echo "Downloading Source Code";
 mkdir -p "visualpay"
-curl https://rawgit.com/oresoftware/quicklock/master/install.sh > "$HOME/.quicklock/ql.sh"
 cd visualpay
+git@github.com:Wuemeli/visualpay.git
 
 echo "To complete installation of 'VisualPay' edit the Config PHP File with your Database Informations";
 echo "Then use the pre-made MySQL Scheme that you find in the install Folder";
