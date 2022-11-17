95c275d5783c0f7cbe5090b7996d686362e06102
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2022-11-17 12:01:56 +0100

    Update

--- Install/install.sh
+++ Install/install.sh
@@ -103,6 +103,27 @@ fi
 
 mysql -u ${username} -p ${dbname} < Install/visualpay.sql
 
+//Creates the Config.PHP File and insert the Database Values
+
+cat > config.php << EOF
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', '${username}');
+define('DB_PASSWORD', '${userpass}');
+define('DB_NAME', '${dbname}');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+EOF
+
 //Starts the Application with php
 php -S ${ip}:${port}
 
