8d2cc8289c5549e00673cd9a9917220787e1b0f6
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2022-11-18 15:32:02 +0100

    Adding the GiftCard Send to Email

--- products.php
+++ products.php
@@ -1,6 +1,12 @@
 <?php
 // Initialize the session
 session_start();
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
@@ -750,6 +756,28 @@ body.dark .home .text{
 										echo "You can't afford it!";
 									} else if($response == 2) {
 										echo "Thanks for the order. Your Giftcard will be sent to your email.";
+                                        $mail = new PHPMailer(true);
+                                        $mail->isSMTP();
+                                        $mail->Host = 'smtp.gmail.com';
+                                        $mail->SMTPAuth = true;
+                                        $mail->Username = '
+[...]
+';
+                                        $mail->Password = '
+[...]
+';
+                                        $mail->SMTPSecure = 'tls';
+                                        $mail->Port = 587;
+                                        $mail->setFrom('
+[...]
+');
+                                        $mail->addAddress($email);
+                                        $mail->isHTML(true);
+                                        $mail->Subject = 'Your Giftcard';
+                                        $mail->Body = 'Your Giftcard Code is: '.$code;
+                                        $mail->send();
+                                    } else if($response == 3) {
+                                        echo "You already bought this product!";
 									}
 								}
 							?>
